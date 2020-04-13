31313dbe2ac459d4733731b2d80668210a9ad807
markmac99 <mark.jm.mcintyre@cesmail.net>
2020-04-13 13:58:59 +0100

    adding ftp settings now password is not needed

---
+++ scripts/ftp-settings.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# FTP/SFTP settings
+IDFILE=/home/pi/.ssh/aws.pem
+IMGDIR='data/'
+MP4DIR='/mnt/mjmm-data/allsky/videos/'
+KEOGRAM_DIR='/mnt/mjmm-data/allsky/keograms/'
+STARTRAILS_DIR='/mnt/mjmm-data/allsky/startrails/'
+USER=bitnami
+HOST=3.9.128.14
