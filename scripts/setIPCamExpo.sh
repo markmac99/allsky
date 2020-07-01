1a7db535115f429b6a3042ebfe14c40ec97cd5f1
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-07-01 19:10:28 +0100

    dvrip needs python3

--- scripts/setIPCamExpo.sh
+++ scripts/setIPCamExpo.sh
@@ -2,6 +2,6 @@
 source /home/pi/allsky/config.sh
 source /home/pi/allsky/scripts/filename.sh
 
-cd  /home/pi
+cd  /home/pi/python-dvr
 
-/usr/bin/python allsky/scripts/SetExpo.py 192.168.1.11 $1
+/usr/bin/python3 /home/pi/allsky/scripts/SetExpo.py 192.168.1.11 $1
