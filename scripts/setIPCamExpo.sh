2bae9a8f8fd89308c501b1fc2c4a25b86fe4365a
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-07-01 19:12:43 +0100

    parameterised ipcam address

--- scripts/setIPCamExpo.sh
+++ scripts/setIPCamExpo.sh
@@ -4,4 +4,4 @@ source /home/pi/allsky/scripts/filename.sh
 
 cd  /home/pi/python-dvr
 
-/usr/bin/python3 /home/pi/allsky/scripts/SetExpo.py 192.168.1.11 $1
+/usr/bin/python3 /home/pi/allsky/scripts/SetExpo.py $IPCAMADDR $1
