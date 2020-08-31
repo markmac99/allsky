40b3fbfb751f2679d9807bd7ef8154e2bd23de16
markmac99 <mark.jm.mcintyre@cesmail.net>
2020-08-31 21:19:41 +0100

    improving network checks

--- scripts/checkNetwork.sh
+++ scripts/checkNetwork.sh
@@ -3,7 +3,7 @@
 export PATH=$PATH:/usr/bin:/usr/sbin:/sbin
 /usr/bin/logger `date +%Y%m%d-%H%M%S` checking network 
 
-ping -c 1 -w 5 192.168.1.254  >/dev/null 2>1
+ping -c 1 -w 5 192.168.1.254  >/dev/null 2>&1
 ret=$?
 if [ $ret -ne 0 ] ; then 
   sleep 10
@@ -11,7 +11,7 @@ if [ $ret -ne 0 ] ; then
   ret=$?
   if [ $ret -ne 0 ] ; then 
     /usr/bin/logger `date +%Y%m%d-%H%M%S` rebooting 
-    /sbin/shutdown -r now 
+    sudo /sbin/shutdown -r now 
   else
     /usr/bin/logger check 2 network up - retcode was $ret
   fi
