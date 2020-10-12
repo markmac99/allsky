39d97f1fa5e9277637e94165835be4e809515b28
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-10-12 22:35:05 +0100

    add location for images

--- scripts/ftp-settings.sh
+++ scripts/ftp-settings.sh
@@ -6,5 +6,6 @@ IMGDIR='data/'
 MP4DIR='/mnt/mjmm-data/allsky/videos/'
 KEOGRAM_DIR='/mnt/mjmm-data/allsky/keograms/'
 STARTRAILS_DIR='/mnt/mjmm-data/allsky/startrails/'
+IMAGES_DIR='/mnt/mjmm-data/allsky/images/'
 USER=bitnami
 HOST=3.9.128.14
