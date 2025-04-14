c5f268fcda9d21dce22dfa14af69e5315f04e3d7
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-04-15 00:44:36 +0100

    use mosquitto to read values as this seems more stable

--- checkAndAlert.sh
+++ checkAndAlert.sh
@@ -9,4 +9,11 @@ srcdir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $srcdir
 source $HOME/miniconda3/bin/activate ukmon-shared
 
-python $srcdir/sendAllskyAlert.py
+BROKER=broker
+MQUSER=mquser
+MQPASS=mqpass
+
+sts=$(mosquitto_sub -h ${BROKER} -u ${MQUSER} -P ${MQPASS} -t meteorcams/allsky/status -i checkallsky -C 1)
+lastupdate=$(mosquitto_sub -h ${BROKER} -u ${MQUSER} -P ${MQPASS} -t meteorcams/allsky/last_update -i checkallsky -C 1)
+
+python -c "from sendAllskyAlert import checkAndSend;checkAndSend($sts, '$lastupdate');"
