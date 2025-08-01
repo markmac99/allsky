68ccee06c25435fdafabc54e39d2bddee852c8d5
Mark McIntyre <markmcintyre99@googlemail.com>
2025-08-01 14:02:42 +0100

    improving youtube upload process

--- youtubeUploader.sh
+++ youtubeUploader.sh
@@ -10,11 +10,14 @@ cd $srcdir
 source ~/vAllsky/bin/activate
 
 apiserver=www.googleapis.com
+oauthserver=oauth2.googleapis.com
 failed=1
 loop=0
 while [ $failed != 0 ] ; do 
-    ping -4 -c 2 -W 10 -w 30 $apiserver # ensure the v4 address is in the DNS cache
-    ping -6 -c 2 -W 10 -w 30 $apiserver # ensure the v6 address is in the DNS cache
+    ping -4 -c 2 -W 10 -w 30 $oauthserver # ensure the v4 address is in the DNS cache
+    ping -6 -c 2 -W 10 -w 30 $oauthserver 
+    ping -4 -c 2 -W 10 -w 30 $apiserver 
+    ping -6 -c 2 -W 10 -w 30 $apiserver 
     failed=$?
     loop=$((loop+1))
     if [ $loop -gt 30 ] ; then 
--- ytUpload.py
+++ ytUpload.py
@@ -9,6 +9,10 @@ from crontab import CronTab
 import ephem
 import socket
 import requests
+import glob
+import logging
+from logging.handlers import RotatingFileHandler
+import configparser
 
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
@@ -18,10 +22,34 @@ from google.auth.transport.requests import Request
 from googleapiclient.errors import HttpError
 
 scopes = ["https://www.googleapis.com/auth/youtube.upload"]
-MINS = 5
+
+log = logging.getLogger('ytUpload')
 
 
-def uploadToYoutube(here, title, fname):
+def updateSentList(here, fname):
+    # reads the list and strip newlines, appends the new entry, 
+    # then makes sure the list is unique and writes it back with newlines
+    if os.path.isfile(os.path.join(here, 'ytdone.txt')):
+        sentlist = open(os.path.join(here, 'ytdone.txt'), 'r').readlines()
+    else:
+        sentlist = []
+    sentlist = [x.strip() for x in sentlist]
+    sentlist.append(fname)
+    sentlist = list(set(sentlist))  
+    open(os.path.join(here, 'ytdone.txt'), 'w').writelines([x + '\n' for x in sentlist])
+    return
+
+
+def checkIfSent(here, fname):
+    # to avoid sending the same video twice
+    sentlist = open(os.path.join(here, 'ytdone.txt'), 'r').readlines()
+    sentlist = [x.strip() for x in sentlist]
+    if fname in sentlist:
+        return True
+    return False
+    
+
+def uploadToYoutube(here, title, fname, forceUpload=False):
     # set to 1 to disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     #os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -34,6 +62,11 @@ def uploadToYoutube(here, title, fname):
     api_version = 'v3'
     client_secrets_file = os.path.join(here, 'client_secret.json')
 
+    if checkIfSent(here, fname) and forceUpload is False:
+        log.info(f'{fname} already uploaded')
+        return False
+    log.info(f'   uploading {fname}')
+    
     credentials = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -74,70 +107,80 @@ def uploadToYoutube(here, title, fname):
     )
     try: 
         status,response = request.next_chunk() # request.execute()
-        print(status, response)
+        log.debug(status)
         if response is not None:
             if 'id' in response:
-                print(f'Video id  {response["id"]} was successfully uploaded.')
+                log.info(f'Video id  {response["id"]} was successfully uploaded.')
                 ret = True
+                updateSentList(here, fname)
             else:
-                print(f'The upload failed with an unexpected response: {response}')
+                log.debug(f'The upload failed with an unexpected response: {response}')
                 ret = False
     except HttpError as e:
-        print(f'HTTP error {e.resp.status} arose with message: {e.content}')
+        log.debug(f'HTTP error {e.resp.status} arose with message: {e.content}')
         ret = False
     socket.setdefaulttimeout(None)
+    if ret is False:
+        log.info('waiting a minute before trying again')
+        sleep(60)
     return ret 
 
 
-def updateCrontab(here, offset=30, lati=51.88, longi=-1.31, elev=80):
+def updateCrontab(here, logdir, offset=30, lati=51.88, longi=-1.31, elev=80):
     obs = ephem.Observer()
     obs.lat = float(lati) / 57.3 # convert to radians, close enough for this
     obs.lon = float(longi) / 57.3
     obs.elev = float(elev)
     obs.horizon = -6.0 / 57.3 # degrees below horizon for darkness
-    
     sun = ephem.Sun()
     rise = obs.next_rising(sun).datetime()
     starttime = rise + datetime.timedelta(minutes=-offset)
-    print('Setting batch start time to', starttime.strftime('%H:%M'))
-
+    log.info(f'Setting batch start time to {starttime.strftime("%H")}')
+    hourstr = f'{starttime.hour},{starttime.hour+1},{starttime.hour+2}'
     cron = CronTab(user=True)
     for job in cron:
         if 'youtubeUploader.sh' in job.command:
             cron.remove(job)
             cron.write()
-    job = cron.new(f'{here}/youtubeUploader.sh > $HOME/logs/ytupload.log 2>&1')
-    job.setall(starttime.minute, starttime.hour, '*', '*', '*')
+    job = cron.new(f'{here}/youtubeUploader.sh > {logdir}/checkYtConn.log 2>&1')
+    job.setall('*/5', hourstr, '*', '*', '*')
     cron.write()
     return
-
-
-def scanLogFile(logname, toddt):
-    lis = open(logname,'r').readlines()
-    tllines = [x for x in lis if 'Timelapse complete' in x]
-    tllines = [x for x in tllines if toddt.strftime('%c')[4:10] in x]
-    if len(tllines) > 0:
-        return True
-    return False
 
 
 if __name__ == "__main__":
     today = datetime.datetime.now()
     here = os.path.split(os.path.abspath(__file__))[0]
-    logfile = sys.argv[1]
+    logfile = sys.argv[1] # not actually used any more
+
+    localcfg = configparser.ConfigParser()
+    localcfg.read(os.path.join(here, 'config.ini'))
+    logdir = localcfg['checking']['logdir']
+    log.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+    fh = RotatingFileHandler(os.path.expanduser(f'{logdir}/ytUploader.log'), maxBytes=10240, backupCount=10)
+    fh.setLevel(logging.INFO)
+    fh.setFormatter(formatter)
+    log.addHandler(fh)
+
     allskyhome = sys.argv[2]
-    yest = today + datetime.timedelta(days=-1)
-    title = f'Allsky timelapse for {yest.strftime("%Y-%m-%d")}'
-    fname = f'allsky-{yest.strftime("%Y%m%d")}.mp4'
-    fname = os.path.join(allskyhome, 'images', f'{yest.strftime("%Y%m%d")}', fname)
-    found = False
-    counter = 0
-    print(f'waiting for {fname}')
-    while not found and counter < MINS*36:
-        found = scanLogFile(logfile, today)
-        if found:
-            print(f'   uploading {fname}')
-            uploadToYoutube(here, title, fname)
-            break
-        sleep(MINS*60)
-    updateCrontab(here)
+    forceUpload = False
+    if len(sys.argv) > 3:
+        forceUpload = True
+
+    log.info('======================')
+    log.info(f'checking for new mp4s at {datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}')
+    allmp4s = glob.glob(os.path.join(os.path.expanduser(allskyhome),'images/**/*.mp4'), recursive=True)
+    allmp4s = list(set(allmp4s))
+    allmp4s.sort()
+    for fname in allmp4s:
+        filedate = os.path.split(fname)[1].split('-')[1][:8]
+        yy = filedate[:4]
+        mm = filedate[4:6]
+        dd = filedate[6:]
+        title = f'Allsky timelapse for {yy}-{mm}-{dd}'
+        ret = uploadToYoutube(here, title, fname, forceUpload)
+        if ret:
+            sleep(60)
+    updateCrontab(here, logdir)
