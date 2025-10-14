581a5d8f59d5159d785f51c99eaa58b88483df7d
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-10-14 22:24:56 +0100

    remove MQ stuff as done by rms_mqtt

--- mqConfig.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Mark McIntyre, 2023-
-
-
-def readConfig():
-    broker = 'someserver.net'
-    username = 'mquser'
-    passwd = 'mqpass'
-    mqport = 1883
-    return broker, mqport, username, passwd
